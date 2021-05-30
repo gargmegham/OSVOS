642db83627465e035815931131ab7c2a261bb9b3
Megham-Garg <33658001+Megham-Garg@users.noreply.github.com>
2021-05-30 18:08:06 +0530

    Update osvos_IoU_score.py

--- osvos_IoU_score.py
+++ osvos_IoU_score.py
@@ -7,6 +7,7 @@ import os
 import sys
 import glob
 import cv2
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, fbeta_score
 root_folder = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(root_folder))
 os.chdir(root_folder)
@@ -30,9 +31,19 @@ def mean_iou_score(annotation_imgs, result_imgs,show_per_frame_iou):
             i += 1
     return iou_score_sum/i
 
+def recall_iou_score(annotation_imgs, result_imgs):
+    i = 1
+    recall_score_img = 0
+    for a_img, r_img in zip(annotation_imgs, result_imgs):
+        a_img = np.array(a_img).reshape((1,np.size(a_img)))[0]
+        r_img = np.array(r_img).reshape((1,np.size(r_img)))[0]
+        recall_score += recall_score(a_img, r_img, pos_label=255)
+    return recall_score_img/i
+
 if __name__ == '__main__':
     sequences = ['car-shadow', 'parkour', 'tennis', 'paragliding-launch', 'drift-chicane', 'bus', 'surf', 'swing', 'breakdance', 'car-roundabout', 'dance-twirl', 'drift-straight', 'breakdance-flare', 'lucia', 'hike', 'hockey', 'bmx-bumps', 'mallard-water', 'car-turn', 'libby', 'dance-jump', 'rollerblade', 'motocross-jump', 'motocross-bumps', 'soapbox', 'drift-turn', 'scooter-black', 'bmx-trees', 'mallard-fly', 'kite-walk', 'paragliding', 'scooter-gray', 'soccerball', 'stroller', 'kite-surf']
     avg_iou = 0
+    recall_iou = 0
     count=len(sequences)
     for seq_name in sequences:
         result_path_BS = os.path.join('DAVIS', 'Results', 'Segmentations', '480p', 'OSVOS', seq_name)
@@ -53,5 +64,8 @@ if __name__ == '__main__':
             result_BS_filenames = [cv2.imread(img,0) for img in result_BS_filenames]
             score = mean_iou_score(annotation_imgs,result_BS_filenames,False)
             avg_iou+=score
+            score = recall_iou_score(annotation_imgs,result_BS_filenames)
+            recall_iou+=score
             
-    print("Mean IOU score for: \n", sequences, "\n is " , avg_iou/count)
+    print("Mean IOU score for: \n", avg_iou/count)
+    print("recall IOU score for: \n", recall_iou/count)
